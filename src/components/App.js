fddeb972b7dc8bde8c21a4b06fc0ca013066c7af
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-09 17:40:48 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = { time: new Date() };
   }
 
-  setTime = () => {
+  setTime() {
     this.setState({ time: new Date() });
-  };
+  }
 
   componentDidMount() {
     this.timer = setInterval(() => this.setTime(), 1000);
@@ -22,6 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="Clock">
+        <h1>hello</h1>
         <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>
       </div>
     );
