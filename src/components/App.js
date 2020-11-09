42c2c5113c900e168976d63f9d46d88130aa752e
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-09 17:31:31 +0530

    changs

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,32 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+    this.state = { time: new Date() };
+
+  }
+
+  setTime = () => {
+    this.setState({ time: new Date() });
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.setTime(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.time}</h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
